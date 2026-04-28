570c963c179fbccc5ea36d7c52fd238c6cfdbaba
nicholascannon <nicholascannon1@gmail.com>
2026-04-28 19:06:09 -0400

    add p10k

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 echo "🔄 Installing configs..."
-stow --target="$HOME" claude tmux vim zsh
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+stow --target="$HOME" \
+  claude \
+  p10k \
+  tmux \
+  vim \
+  zsh
+
 ln -sf $(pwd)/ghostty/config "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
+
 echo "✅ done"

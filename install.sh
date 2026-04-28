3f0af309f7711a1616ee19cbb0814161abbace9c
nicholascannon <nicholascannon1@gmail.com>
2026-04-28 19:36:04 -0400

    fix install and p10k

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ echo "🔄 Installing configs..."
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
-stow --target="$HOME" \
+stow --target="$HOME" -R --verbose \
   claude \
   p10k \
   tmux \
